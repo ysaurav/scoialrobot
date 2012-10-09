06194a40f0a8837cbbfbe69143e7cb3f985b9f00
corina.barbalata@gmail.com <corina.barbalata@gmail.com@1f800816-4cb8-6287-cebc-c228405b4579>
2012-10-09 20:31:09 +0000

    preprocessing

--- trunk/social_robot/src/preprocessing.cpp
+++ trunk/social_robot/src/preprocessing.cpp
@@ -16,14 +16,22 @@ int main(int argc, char **argv)
   cv::Mat rgb_im = cv::imread(argv[1], CV_LOAD_IMAGE_COLOR);
   cv::Mat depth_im = cv::imread(argv[2], CV_LOAD_IMAGE_COLOR);
 
-  // do processing
+  cv::Mat dst ;
+  cv::Mat temp;
 
+  //std::cout << "ama"<<std:endl;
+  // do processing
+   cv::resize(depth_im, temp, cv::Size(), 2, 2, 1);
+   cv::resize(temp, dst, cv::Size(), 0.5, 0.5, 1);
+
+   cv::medianBlur(dst, dst,5);
+  
   // write the result to the result folder
   std::string depth_im_result = ros::package::getPath("social_robot");
   depth_im_result.append("/logs/preprocessed_");
   depth_im_result.append(argv[2]);
-  bool imwrite_result = cv::imwrite(depth_im_result, depth_im);
-  std::cout << depth_im_result << " was " << imwrite_result << std::endl;
+  bool imwrite_result = cv::imwrite(depth_im_result, temp);
+  //std::cout << depth_im_result << " was " << imwrite_result << std::endl;
   
   return 0;
 }
