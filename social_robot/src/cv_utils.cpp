b9a19b099a098adb26f1231c5b7e5605c04e0139
akbarinia.arash <akbarinia.arash@gmail.com>
2012-10-14 15:09:17 +0000

    Finished chamfer matching.

--- social_robot/src/cv_utils.cpp
+++ social_robot/src/cv_utils.cpp
@@ -2,11 +2,37 @@
 
 using namespace cv;
 
-Mat rgb2bw(cv::Mat im_rgb)
+Mat rgb2bw(Mat im_rgb)
 {
   Mat im_gray;
-  cvtColor(im_rgb, im_gray, CV_RGB2GRAY);
+  if (im_rgb.channels() == 3)
+  {
+    cvtColor(im_rgb, im_gray, CV_RGB2GRAY);
+  }
+  else
+  {
+    im_gray = im_rgb;
+  }
+  
   Mat im_bw;
-  threshold(im_gray, im_bw, 128, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
+  threshold(im_gray, im_bw, 128, 255, CV_THRESH_BINARY);
   return im_bw;
+}
+
+void get_non_zeros(Mat img, std::vector<cv::Point> *points, cv::Point pdiff, double scale)
+{
+  int k = 0;
+  for (int i = 0; i < img.rows; i++)
+  {
+    float *rowi = img.ptr<float>(i);
+    for (int j = 0; j < img.cols; j++)
+    {
+      if (rowi[j] != 0)
+      {
+        cv::Point point = (cv::Point(j, i) + pdiff) * scale;
+        points->push_back(point);
+        k++;
+      }
+    }
+  }
 }
