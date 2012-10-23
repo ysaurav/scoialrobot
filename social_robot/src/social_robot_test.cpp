2d361cd2f6c242b162abf874774d36baf2487bed
akbarinia.arash <akbarinia.arash@gmail.com>
2012-10-23 17:57:46 +0000

    Social Robot Test for one image.

--- social_robot/src/social_robot_test.cpp
+++ social_robot/src/social_robot_test.cpp
@@ -6,8 +6,10 @@
 #define HEAD_TEMPLATE "pictures/template.png"
 #define THR1          5
 #define THR2          7
-#define THR3          20
+#define THR3          10
 #define SCALES        5
+
+cv::Mat canny_im;
 
 cv::Mat preprocessing(cv::Mat image)
 {
@@ -15,16 +17,32 @@ cv::Mat preprocessing(cv::Mat image)
   cv::Mat temp;
   cv::resize(image, temp, cv::Size(), 2, 2, cv::INTER_NEAREST);
   cv::resize(temp, dst, cv::Size(), 0.5, 0.5, cv::INTER_NEAREST);
-  cv::medianBlur(dst, dst, 5);
-  // cv::namedWindow("Preprocessing", CV_WINDOW_AUTOSIZE);
-  // cv::imshow("Preprocessing", dst);
-  // cv::waitKey(0);
+  // inpainting
+  cv::Mat mask = cv::Mat::zeros(image.rows, image.cols, CV_8U); 
+  cv::Mat temp2(image.rows, image.cols, CV_8U);
+  cv::Mat temp3(image.rows, image.cols, CV_8U);
+  for (int i = 0; i < image.rows; i++)
+  {
+    for (int j = 0; j < image.cols; j++)
+    {
+      if (image.at<unsigned char>(i, j) == 0)
+      {
+        mask.at<unsigned char>(i, j) = 255;
+      }
+    }
+  }
+  bilateralFilter(image, temp3, 1, 0.01, 0.1);
+  cv::medianBlur(temp3, temp3, 7);
+  inpaint(temp3, mask, temp2, 10, cv::INPAINT_NS);
+  cv::medianBlur(temp2, dst, 7);
+  imshow("Preprocessing", dst);
+  cv::waitKey(0);
   return dst;
 }
 
 std::vector<cv::Point> chamfer_matching(cv::Mat image)
 {
-  cv::Mat canny_im(image.rows, image.cols, image.depth());
+  canny_im.create(image.rows, image.cols, image.depth());
   cv::Mat template_im = cv::imread(HEAD_TEMPLATE, CV_LOAD_IMAGE_ANYDEPTH);
 
   cv::Mat* pyramid = new cv::Mat[SCALES];
@@ -33,6 +51,8 @@ std::vector<cv::Point> chamfer_matching(cv::Mat image)
 
   // calculate edge detection
   cv::Canny(image, canny_im, THR1, THR2, 3, true);
+  imshow("Canny", canny_im);
+  cv::waitKey(0);
 
   // calculate the Canny pyramid
   pyramid[0] = canny_im;
@@ -62,7 +82,7 @@ std::vector<cv::Point> chamfer_matching(cv::Mat image)
 
   for (int i = 0; i < SCALES; i++)
   {
-    matchTemplate(chamfer[i], template_im, matching[i], CV_TM_CCOEFF);
+    matchTemplate(chamfer[i], template_im, matching[i], CV_TM_CCOEFF_NORMED);
     normalize(matching[i], matching[i], 0.0, 1.0, cv::NORM_MINMAX);
     cv::minMaxLoc(matching[i], &minVal, &maxVal, &minLoc, &maxLoc);
     cv::Mat matching_thr;
@@ -74,89 +94,77 @@ std::vector<cv::Point> chamfer_matching(cv::Mat image)
   return head_matched_points;
 }
 
-std::vector<cv::Point3f> headparameters(cv::Mat image, std::vector<cv::Point> chamfer)
+std::vector<cv::Point3f> compute_headparameters(cv::Mat image, std::vector<cv::Point> chamfer)
 {
-  int position_x;
-  int position_y;
-  unsigned short d;
-  unsigned short x;
+  std::vector<cv::Point3f> parameters_head(chamfer.size());
 
-  std::vector<cv::Point3f> parametersHead(chamfer.size());
-  float h;
-  float R;
-  float Rp;
-  //parameters of cubic equation
-
-  float p1 = -1.3835*pow(10,-9);
-  float p2 = 1.8435*pow(10,-5);
+  // parameters of cubic equation
+  float p1 = -1.3835 * pow(10, -9);
+  float p2 =  1.8435 * pow(10, -5);
   float p3 = -0.091403;
-  float p4 = 189.38;
+  float p4 =  189.38;
 
-  // depthImage.convertTo(temp, CV_32F);
   for (unsigned int i = 0; i < chamfer.size(); i++)
   {
-    // std::cout<<"Here"<<std::endl;
-    position_x = chamfer[i].x;
-    position_y = chamfer[i].y;
-    //  std::cout<< "x "<< position_x<<" y "<<position_y<<std::endl;
+    int position_x = chamfer[i].x;
+    int position_y = chamfer[i].y;
 
-    d = image.at<unsigned short>(position_x,position_y);
-    // x = (1/(alpha*(d - beta)))*1000;
-    // std::cout<< "Depth of pixel"<< image.at<unsigned short>(position_y,position_x)<<std::endl;
-    //  std::cout<< "Depth of pixel "<< d <<std::endl;
-
-    x = image.at<unsigned short>(position_x,position_y);
+    unsigned short x = image.at<unsigned short>(position_y, position_x);
+    
     // compute height of head
-    h = (p1*pow(x,3) + p2*pow(x,2) + p3*x + p4);
+    float h = (p1 * pow(x, 3) + p2 * pow(x, 2) + p3 * x + p4);
 
     // compute Radius of head in milimeters
-    R = 1.33*h/2;
+    float R = 1.33 * h / 2;
 
     // convert Radius in pixels
-    Rp = round((1/1.3)*R);
+    float Rp = round((1 / 1.3) * R);
 
-    // std::cout << "Height of head is: " << h << " Radius in milimeters is: " << R << " Radius in pixels: "<< Rp <<std::endl;
-
-    parametersHead[i].x = h;
-    parametersHead[i].y = R;
-    parametersHead[i].z = Rp;
+    parameters_head[i].x = position_x;
+    parameters_head[i].y = position_y;
+    parameters_head[i].z = Rp;
   }
 
-  return parametersHead;
+  return parameters_head;
 }
-
 
 int main(int argc, char **argv)
 {
-  //  ros::init(argc, argv, "social_robot");
-  //  ros::spin();
-
-  // temporarily reading the image from file later on change it to the live video.
   cv::Mat image_dispa = cv::imread(argv[1], CV_LOAD_IMAGE_ANYDEPTH);
   cv::Mat image_depth = cv::imread(argv[2], CV_LOAD_IMAGE_ANYDEPTH);
   cv::Mat image_rgb = cv::imread(argv[3], CV_LOAD_IMAGE_ANYDEPTH);
-  // cvtColor(image, image, CV_RGB2GRAY);
   image_dispa = preprocessing(image_dispa);
-  image_depth = preprocessing(image_depth);
+//  image_depth = preprocessing(image_depth);
   std::vector<cv::Point> head_matched_points = chamfer_matching(image_dispa);
+   
+  int thr = atoi(argv[4]); // default must be 12
+  int thr2 = atoi(argv[5]); // default must be 17
 
-  std::vector<cv::Point3f> parametersHead = headparameters(image_depth,head_matched_points);
-  int th = 20;
-
-  cv::Point center;
-  cv::Scalar color( 255, 255, 255 );
-  float Rp;
-  for (unsigned int i = 0; i < parametersHead.size(); i++)
+  std::vector<cv::Point3f> tmpparams = compute_headparameters(image_depth, head_matched_points);
+  for (unsigned int i = 0; i < tmpparams.size(); i++)
   {
-    Rp = parametersHead[i].z;
-    center.x = head_matched_points[i].x;
-    center.y = head_matched_points[i].y;
-    circle(image_rgb, center, Rp, color, 1, 2);
+//    cv::circle(image_rgb, cv::Point(tmpparams[i].x, tmpparams[i].y), tmpparams[i].z, cvScalar(255, 255, 0, 0), 2, 8, 0);
+    cv::Rect roi(tmpparams[i].x - tmpparams[i].z, tmpparams[i].y - tmpparams[i].z, tmpparams[i].z * 2, tmpparams[i].z * 2);
+    if (!(0 <= roi.x && 0 <= roi.width && roi.x + roi.width <= canny_im.cols && 0 <= roi.y && 0 <= roi.height && roi.y + roi.height <= canny_im.rows))
+    {
+      continue;
+    }
+    cv::Mat tmp_mat = canny_im(roi);
+    std::vector<std::vector<cv::Point> > contour;
+    cv::findContours(tmp_mat, contour, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+    for (unsigned int j = 0; j < contour.size(); j++)
+    {
+      std::vector<cv::Point> approx;
+      cv::approxPolyDP(contour[j], approx, 5, false);          
+      if (approx.size() > thr && approx.size() < thr2)
+      {
+        rectangle(image_rgb, roi.tl(), roi.br(), cvScalar(255, 0, 255, 0), 2, 8, 0);
+        break;
+      }
+    }
   }
-
-  cv::namedWindow("Image", CV_WINDOW_AUTOSIZE);
-  cv::imshow("Image", image_rgb);
+  // imwrite("circle_img.png", image_rgb);
+  cv::imshow("HOla", image_rgb);
   cv::waitKey(0);
-
   return 0;
 }
