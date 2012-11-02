28707c1d4ad4cd488ed944c51f06b7400dc59ca9
akbarinia.arash <akbarinia.arash@gmail.com>
2012-11-02 17:11:20 +0000

    Reading openni files.

---
+++ openni_ros/openni_camera_deprecated/build/CMakeFiles/openni_deprecated.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/openni_deprecated.dir/src/nodelets/openni_nodelet.o"
+  "../lib/libopenni_deprecated.pdb"
+  "../lib/libopenni_deprecated.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/openni_deprecated.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
