e1a7433a886c1ce11f63250c8ffe957844f5773a
akbarinia.arash <akbarinia.arash@gmail.com>
2012-11-15 16:36:00 +0000

    Separated RGB thread.

--- social_robot/src/SocialRobotGui.cpp
+++ social_robot/src/SocialRobotGui.cpp
@@ -35,7 +35,7 @@ void SocialRobotGui::init()
   ros::start(); // our node handles go out of scope, so we want to control shutdown explicitly.
   ros::NodeHandle nh;
   rgb_subscriber = nh.subscribe ( "/camera/rgb/image_color", 1, &SocialRobotGui::rgb_cb, this );
-  rgb_rois_subs = nh.subscribe ( "/social_robot/rgb_rois", 1, &SocialRobotGui::rgb_rois_cb, this );
+  rgb_rois_subs = nh.subscribe ( "/social_robot/rgb/rois", 1, &SocialRobotGui::rgb_rois_cb, this );
   depth_rois_subs = nh.subscribe ( "/social_robot/depth/rois", 1, &SocialRobotGui::depth_rois_cb, this );
   start();
 }
@@ -50,6 +50,7 @@ void SocialRobotGui::rgb_cb ( const sensor_msgs::ImageConstPtr &msg )
   try
     {
       Mat image_rgb = cv_bridge::toCvCopy ( msg, enc::BGR8 )->image;
+      draw_rgb_faces ( image_rgb, rgb_rois );
       draw_depth_faces ( image_rgb, depth_rois );
       emit update_image ( image_rgb );
     }
