b18a41c6b732f3a4e13f9a2f49ad0f141bc4f929
akbarinia.arash@gmail.com <akbarinia.arash@gmail.com@1f800816-4cb8-6287-cebc-c228405b4579>
2012-11-19 22:45:45 +0000

    Particle filter added.

--- trunk/social_robot/src/social_robot_rgb.cpp
+++ trunk/social_robot/src/social_robot_rgb.cpp
@@ -5,6 +5,10 @@
 #include "kinect_proxy.h"
 #include "cv_utils.h"
 #include "RosUtils.h"
+#include "particle_filter/StateData.h"
+#include "particle_filter/hist.h"
+#include "particle_filter/filter.h"
+#include "particle_filter/lbp.h"
 
 using namespace std;
 using namespace cv;
@@ -20,9 +24,13 @@ RosUtils ros_utils;
 social_robot::RegionOfInterests rgb_pub_rois;
 ros::Publisher rgb_pub;
 
+bool is_tracking = false;
+vector<StateData> state_datas(2);
+unsigned int numpeople = 0;
+
 vector<Rect> detect_face_rgb ( Mat img, CascadeClassifier &cascade )
 {
-  vector<Rect> tmpfaces;
+  vector<Rect> detected_faces;
 
   Mat gray;
   Mat frame ( cvRound ( img.rows ), cvRound ( img.cols ), CV_8UC1 );
@@ -31,7 +39,7 @@ vector<Rect> detect_face_rgb ( Mat img, CascadeClassifier &cascade )
   resize ( gray, frame, frame.size(), 0, 0, INTER_LINEAR );
   equalizeHist ( frame, frame );
 
-  cascade.detectMultiScale ( frame, tmpfaces,
+  cascade.detectMultiScale ( frame, detected_faces,
                              1.1, 2, 0
                              //|CV_HAAR_FIND_BIGGEST_OBJECT
                              //|CV_HAAR_DO_ROUGH_SEARCH
@@ -39,10 +47,10 @@ vector<Rect> detect_face_rgb ( Mat img, CascadeClassifier &cascade )
                              ,
                              Size ( 30, 30 ) );
 
-  vector<RegionOfInterest> rosrois = ros_utils.cvrects2rosrois ( tmpfaces );
+  vector<RegionOfInterest> rosrois = ros_utils.cvrects2rosrois ( detected_faces );
   rgb_pub_rois.rois.swap ( rosrois );
   rgb_pub.publish ( rgb_pub_rois );
-  return tmpfaces;
+  return detected_faces;
 }
 
 bool update_param_cb ( std_srvs::Empty::Request&, std_srvs::Empty::Response& )
@@ -85,7 +93,43 @@ void rgb_cb ( const ImageConstPtr& msg )
   try
     {
       Mat image_rgb = cv_bridge::toCvCopy ( msg, enc::BGR8 )->image;
-      vector<Rect> rgbfacestmp = detect_face_rgb ( image_rgb, classifier );
+      Mat gray;
+      if ( is_tracking )
+        {
+          for ( unsigned int i = 0; i < numpeople; i++ )
+            {
+              image_rgb.copyTo ( state_datas[i].image );
+              // Set up all the image formats we'll need
+              if ( state_datas[i].use_lbp )
+                {
+                  cvtColor ( state_datas[i].image, gray, CV_BGR2GRAY );
+                  lbp_from_gray ( gray, state_datas[i].lbp );
+                }
+              else
+                {
+                  if ( state_datas[i].lbp.empty() )
+                    {
+                      state_datas[i].lbp = Mat::zeros ( state_datas[i].image.rows, state_datas[i].image.cols, CV_8UC1 );
+                    }
+                }
+
+              state_datas[i].tracking();
+            }
+        }
+      else
+        {
+          vector<Rect> rgbfaces = detect_face_rgb ( image_rgb, classifier );
+          numpeople = rgbfaces.size();
+          for ( unsigned int i = 0; i < rgbfaces.size(); i++ )
+            {
+              state_datas[i].initialise( 200, false, image_rgb, rgbfaces[i] );
+              state_datas[i].tracking();
+              is_tracking = true;              
+            }            
+        }
+      draw_tracking_faces( image_rgb, state_datas );
+      imshow ( "Arash", image_rgb );
+      waitKey ( 3 );
     }
   catch ( cv_bridge::Exception& e )
     {
@@ -119,6 +163,9 @@ int main ( int argc, char **argv )
     {
       return -1;
     }
+
+  // particle filter initialisaiton  
+  lbp_init();
 
   // subscribtions
   ros::ServiceServer update_srv = nh.advertiseService ( "/social_robot/update", update_param_cb );
