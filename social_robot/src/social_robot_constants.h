06bec8cfcd1a5d92dd07ae5e451e6a069c1f018b
rgilrodr7 <rgilrodr7@gmail.com>
2012-11-20 12:27:24 +0000

    New header for constants

---
+++ social_robot/src/social_robot_constants.h
@@ -0,0 +1,16 @@
+
+#define ROIS "/social_robot/depth/rois"
+#define DEPTH_IMAGE "/camera/depth/image_raw"
+#define DISPARITY_IMAGE "/camera/depth/disparity"
+#define RGB_UPDATE "/social_robot/depth/update"
+
+#define MATCH3D_THR "/social_robot/depth/match3D_thr"
+#define MAX_SUPPRESSION "/social_robot/depth/max_suppression"
+#define ARC_THR_HIGH "/social_robot/depth/arc_thr_high"
+#define ARC_THR_LOW "/social_robot/depth/arc_thr_low"
+#define SCALES "/social_robot/depth/scales"
+#define CHAMFER_THR "/social_robot/depth/chamfer_thr"
+#define DEPTH_REGISTRATION "/camera/driver/depth_registration"
+
+#define SOCIAL_ROBOT_DEPTH "social_robot_depth"
+#define SOCIAL_ROBOT "social_robot"
